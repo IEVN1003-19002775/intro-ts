7e54c017a243c4d08d82e158fbb59fd34ea8833e
Joan Morales <19002775@alumnos.utleon.edu.mx>
2024-09-06 18:50:00 -0600

    06/09/24

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,21 @@
+var num1 = 38
+let num2 = 29
+const num3 = 10
+
+let sum = num1 + num2 + num3;
+  
+let nombre:string
+let num:number
+let activo:boolean
+
+nombre = '23'
+num = 32
+activo = true
+
+let matricula:number|string|boolean
+
+matricula = 1234567
+matricula = 'GHGVFHJFCF'
+
+
+console.log('El número es ',sum);
